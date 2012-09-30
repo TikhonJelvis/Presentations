f4f55d209f42d950f88e1d09d522f2dbb2731b83
Tikhon Jelvis <tikhon@berkeley.edu>
2012-09-29 18:20:38 -0700

    Added a second test that outputs HTML.

--- src/Presentations/Main.hs
+++ src/Presentations/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import           Data.Functor                  ((<$>))
 import           Data.Tree                     (drawForest)
 
+import           Presentations.Output
 import           Presentations.Parse
 
 import           Text.ParserCombinators.Parsec (parseFromFile)
@@ -10,9 +11,15 @@ import           Text.ParserCombinators.Parsec (parseFromFile)
 exampleFile = "/home/tikhon/Documents/programming/haskell/presentations/example.org"
 
 test :: IO ()
-test = do readFile exampleFile >>= putStrLn
-          parsed <- parseFromFile outline exampleFile
+test = do parsed <- parseFromFile outline exampleFile
           case parsed of
             Left e -> putStrLn "Error!" >> print e
             Right v -> do print v
-                          putStrLn $ drawForest (fmap show <$> toTree v)
+                          putStrLn $ drawForest (fmap show <$> v)
+
+test2 :: IO ()
+test2 = do parsed <- parseFromFile outline exampleFile
+           case parsed of
+             Left e -> putStrLn "Error!" >> print e
+             Right v -> do print v
+                           mapM_ (putStrLn . slideToDiv) v
