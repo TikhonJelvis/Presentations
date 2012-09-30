47c156cd363cd130ce4d72ae7e3af383450bbdbc
Alice Lee <alice.lee.92@gmail.com>
2012-09-29 22:02:43 -0700

    font features and title

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 	var p  = new Raphael(0,0,960,600);
 	var r = p.rect(0,0,960,600);
+	r.attr(  
+        {  
+            gradient: '90-#E80000-#780000',  
+            stroke: '#3b4449',  
+            'stroke-width': 10,  
+            'stroke-linejoin': 'round',  
+            rotation: -90  
+        });
 	var prev = p.rect(0,0,280,600).attr({opacity: '0', fill: "#000"});
 	var next = p.rect(680, 0, 280, 600).attr({opacity: '0', fill:"#000"});
 	prev.node.setAttribute("class", "prev arrow");
@@ -38,15 +46,42 @@ $(document).ready(function() {
 	}
 
 	function displayBullets(current_slide){
+		var title = p.text( 480, 50, current_slide.find('.title').text()).attr({"font-size": "50px"});
 		var top_y = 100;
 		var increment = 30;
 		//one bullet point
 		current_slide.find('li').each(function(){
-			var t = p.text(50, top_y, "").attr('text-anchor', 'start');
-			t.node.setAttribute("class", "bullet_point " + $(this).attr('class'));
-			var width = 860;
-			textWrap(t, width, $(this).text());
-	 		top_y += t.getBBox().height + 10;
+			var prev_text = p.text(50,top_y,"");
+			$(this).contents().each(function(){
+				if ($(this).is('span')) {
+					//figure out type of text
+					var t = p.text(prev_text.getBBox().width + prev_text.getBBox().x + 1, top_y, "").attr('text-anchor', 'start');
+					t.node.setAttribute("class", "bullet_point ");
+					var c = $(this).attr('class') || "";
+					if (c.indexOf('bold') >=0){
+						t.attr({"font-weight": "700"});
+					}
+					if (c.indexOf('italic') >= 0){
+						t.attr("font-style", "italic");
+					}
+					if (c.indexOf('underline') >= 0){
+						t.attr("text-decoration", "underline");
+					}
+					if (c.indexOf('code') >= 0) {
+						t.attr("font-family", "monospace");
+					}
+					var width = 860 - prev_text.getBBox().width;
+					textWrap(t, width, $(this).html());
+					prev_text = t;
+				} else {
+					var t = p.text(prev_text.getBBox().width + prev_text.getBBox().x + 1, top_y, "").attr('text-anchor', 'start');
+					t.node.setAttribute("class", "bullet_point ");
+					var width = 860 - prev_text.getBBox().width;
+					textWrap(t, width, $(this).text());
+					prev_text = t;
+				}
+			});
+	 		top_y = prev_text.getBBox().y + prev_text.getBBox().height + 90;
 		});
 	}
 
@@ -55,7 +90,8 @@ $(document).ready(function() {
 		//to determine average letter length
 		var abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		t.attr({
-			"text" : abc
+			"text" : abc,
+			"font-size" : "30px"
 		});
 		var letterWidth = t.getBBox().width / abc.length;
 
